75e6ff7ce7149f260ebd80d7a5c4a9ab1d296712
Matthew So <96310058+stereoscoped@users.noreply.github.com>
2025-03-17 22:25:03 -0700

    Make it such that random index doesnt always equal 2

--- 4. Hangman/main.cpp
+++ 4. Hangman/main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <random>
 
 int main() {
-    std::cout << "HELP MEE" << endl;
+    std::string words[5] = {"hello","bye","snake","rat","apple"};
+    char guess = ' ';
+    int guessNumber = 1;
+    int incorrectCount = 0;
+    int incorrectLimit = 8;
+
+    char word[50];
+    strcpy(word, words[rand()%size(words)].c_str());
+    while (incorrectCount < incorrectLimit) {
+        bool incorrect = true;
+        std::cout << "Guess " << guessNumber << ": ";
+        std::cin >> guess;
+        for (int i = 0; i < sizeof(word); ++i) {
+            if (guess == word[i]) {
+                std::cout << "Correct!" << std::endl;
+                incorrect = false;
+            }
+        }
+        if (incorrect) {
+            std::cout << "Incorrect!" << std::endl;
+            ++incorrectCount;
+        }
+        ++guessNumber;
+    }
+
 }
